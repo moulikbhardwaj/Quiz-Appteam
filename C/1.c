651b46e8a48ba68590c9798599013ad531251b7b
SArthak3661 <guptasarthak451@gmail.com>
2019-10-20 22:16:27 +0530

    Added

---
+++ C/1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int miain()
+{   int a,b,c,d,e,m;
+    printf("Enter number a:");
+    scanf("%d",&a);
+ printf("Enter number b:");
+    scanf("%d",&b);
+ printf("Enter number c:");
+    scanf("%d",&c);
+ printf("Enter number d:");
+    scanf("%d",&d);
+ printf("Enter number e:");
+    scanf("%d",&e);
+    m = (a*b)*(c*(d*e));
+    printf("result",m);
+    return 0;
+}
